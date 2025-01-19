0c0c6a9cefb94476e2314430d5a44d6f59dd4dad
Paul <paul.matros@gmail.com>
2025-01-20 00:51:28 +0100

    data.sql updated for int price values

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,19 +4,19 @@ INSERT INTO cuisine (id, name) VALUES (2, 'Mexican');
 INSERT INTO cuisine (id, name) VALUES (3, 'Italian');
 
 -- Insert Menu Items (Main courses)
-INSERT INTO menu_item (id, name, price, type, cuisine_id) VALUES (1, 'Pierogi', 10.0, 'MAIN_COURSE', 1);
-INSERT INTO menu_item (id, name, price, type, cuisine_id) VALUES (2, 'Tacos', 12.5, 'MAIN_COURSE', 2);
-INSERT INTO menu_item (id, name, price, type, cuisine_id) VALUES (3, 'Lasagna', 14.0, 'MAIN_COURSE', 3);
+INSERT INTO menu_item (id, name, price, type, cuisine_id) VALUES (1, 'Pierogi', 1000, 'MAIN_COURSE', 1);
+INSERT INTO menu_item (id, name, price, type, cuisine_id) VALUES (2, 'Tacos', 1250, 'MAIN_COURSE', 2);
+INSERT INTO menu_item (id, name, price, type, cuisine_id) VALUES (3, 'Lasagna', 1400, 'MAIN_COURSE', 3);
 
 -- Insert Menu Items (Desserts)
-INSERT INTO menu_item (id, name, price, type, cuisine_id) VALUES (4, 'Polish Cheesecake', 6.0, 'DESSERT', 1);
-INSERT INTO menu_item (id, name, price, type, cuisine_id) VALUES (5, 'Churros', 5.5, 'DESSERT', 2);
-INSERT INTO menu_item (id, name, price, type, cuisine_id) VALUES (6, 'Tiramisu', 7.0, 'DESSERT', 3);
+INSERT INTO menu_item (id, name, price, type, cuisine_id) VALUES (4, 'Polish Cheesecake', 600, 'DESSERT', 1);
+INSERT INTO menu_item (id, name, price, type, cuisine_id) VALUES (5, 'Churros', 550, 'DESSERT', 2);
+INSERT INTO menu_item (id, name, price, type, cuisine_id) VALUES (6, 'Tiramisu', 700, 'DESSERT', 3);
 
 -- Insert Menu Items (Drinks)
-INSERT INTO menu_item (id, name, price, type) VALUES (7, 'Water', 1.0, 'DRINK');
-INSERT INTO menu_item (id, name, price, type) VALUES (8, 'Soda', 2.5, 'DRINK');
+INSERT INTO menu_item (id, name, price, type) VALUES (7, 'Water', 100, 'DRINK');
+INSERT INTO menu_item (id, name, price, type) VALUES (8, 'Soda', 25, 'DRINK');
 
 -- Insert Menu Items (Additions)
-INSERT INTO menu_item (id, name, price, type) VALUES (10, 'Ice', 0.5, 'DRINK_ADDITION');
-INSERT INTO menu_item (id, name, price, type) VALUES (11, 'Lemon', 0.75, 'DRINK_ADDITION');
+INSERT INTO menu_item (id, name, price, type) VALUES (10, 'Ice', 0, 'DRINK_ADDITION');
+INSERT INTO menu_item (id, name, price, type) VALUES (11, 'Lemon', 75, 'DRINK_ADDITION');
