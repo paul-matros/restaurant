58b809c538a79c2af1854c2279d6fc4347707c2b
Paul <paul.matros@gmail.com>
2025-01-20 00:53:28 +0100

    order canncelled message added and order summary added to shell

--- src/main/java/pl/matros/restaurant/shell/InteractiveOrderShell.java
+++ src/main/java/pl/matros/restaurant/shell/InteractiveOrderShell.java
@@ -3,118 +3,29 @@ package pl.matros.restaurant.shell;
 import lombok.RequiredArgsConstructor;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
-import pl.matros.restaurant.model.*;
-import pl.matros.restaurant.service.InputHandler;
+import pl.matros.restaurant.model.OrderItem;
+import pl.matros.restaurant.model.PlacedOrder;
+import pl.matros.restaurant.service.InteractiveWaiter;
 import pl.matros.restaurant.service.OrderService;
 
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Optional;
 
 @ShellComponent
 @RequiredArgsConstructor
 public class InteractiveOrderShell {
 
     private final OrderService orderService;
-    private final InputHandler inputHandler;
+    private final InteractiveWaiter interactiveWaiter;
 
-    @ShellMethod("Say hello")
-    public String hello() {
-        return "Hello, world!";
-    }
-
-    @ShellMethod(key = "order", value = "order lunch")
+    @ShellMethod(key = "order", value = "Place a new order")
     public String order() {
-        List<OrderItem> order = new LinkedList<>();
-        if (inputHandler.getYesNoAnswer("Do you wish to order lunch?")) {
-            System.out.println("Ordering lunch...");
-            List<OrderItem> lunch = orderLunch();
-            order.addAll(lunch);
+        List<OrderItem> orderItems = interactiveWaiter.takeOrder();
+        if (orderItems.isEmpty()) {
+            return "Order cancelled - no items selected.";
         }
-        if (inputHandler.getYesNoAnswer("Do you wish to order drink?")) {
-            OrderItem drink = orderDrink();
-            order.add(drink);
-        }
-        //orderService.placeOrder(order);
-        return (order.toString());
-    }
-
-    private OrderItem orderDrink() {
-        MenuItem drink = selectMenuItem(MenuItemType.DRINK);
-        List<MenuItem> orderedAdditions = new LinkedList<>();
-        if (inputHandler.getYesNoAnswer("Do you wish to add something to your drink?")) {
-            List<MenuItem> drinkAdditions = orderService.getMenuItemsByType(MenuItemType.DRINK_ADDITION);
-            MenuItem selectedAddition;
-            do {
-                selectedAddition = selectMenuItem(drinkAdditions, "What would you like to add to your drink?");
-                orderedAdditions.add(selectedAddition);
-                drinkAdditions.remove(selectedAddition);
-            } while (selectedAddition != null && drinkAdditions.size() > 0);
-        }
-        return new OrderItem(drink, orderedAdditions);
-    }
-
-    private List<OrderItem> orderLunch() {
-        List<OrderItem> lunch = new LinkedList<>();
-        lunch.add(orderMainCourse());
-        lunch.add(orderDessert());
-        return lunch;
-    }
-
-    private OrderItem orderDessert() {
-        return new OrderItem(selectMenuItem(MenuItemType.DESSERT));
-    }
-
-    private OrderItem orderMainCourse() {
-        Cuisine cuisine = selectCuisine();
-        OrderItem mainCourse;
-        if (cuisine == null) {
-            mainCourse = new OrderItem(selectMenuItem(MenuItemType.MAIN_COURSE));
-        } else {
-            mainCourse = new OrderItem(selectMenuItem(MenuItemType.MAIN_COURSE, cuisine));
-        }
-        return mainCourse;
-    }
-
-    private Cuisine selectCuisine() {
-        List<Cuisine> cuisines = orderService.getAllCuisines();
-        Optional<Displayable> cuisine = selectDisplayableItem(cuisines, "cuisine");
-        return (Cuisine) cuisine.orElse(null);
-    }
-
-    private MenuItem selectMenuItem(MenuItemType menuItemType, Cuisine cuisine) {
-        List<MenuItem> menuItems = orderService.getMenuItemsByCuisineAndType(cuisine, menuItemType);
-        String menuTitle = String.format("Select %s menu item of %s cuisine", menuItemType.name(), cuisine.getDisplayName());
-        return selectMenuItem(menuItems, menuTitle);
-    }
-
-    private MenuItem selectMenuItem(MenuItemType menuItemType) {
-        List<MenuItem> menuItems = orderService.getMenuItemsByType(menuItemType);
-        String menuTitle = String.format("Select %s menu item", menuItemType.name());
-        return selectMenuItem(menuItems, menuTitle);
-    }
-
-    private MenuItem selectMenuItem(List<MenuItem> menuItems, String menuTitle) {
-        Optional<Displayable> menuItem = selectDisplayableItem(menuItems, menuTitle);
-        return (MenuItem) menuItem.orElse(null);
-    }
-
-    private Optional<Displayable> selectDisplayableItem(List<? extends Displayable> items, String menuTitle) {
-        this.printDisplayableMenu(items, menuTitle);
-
-        int selected = inputHandler.readIntInRange(0, items.size());
-
-        if (selected == 0) {
-            return Optional.empty();
-        }
-        return Optional.of(items.get(selected - 1));
-    }
-
-    private void printDisplayableMenu(List<? extends Displayable> items, String menuTitle) {
-        System.out.println(menuTitle + "(or 0 to skip):");
-        for (int i = 0; i < items.size(); i++) {
-            System.out.println((i + 1) + ". " + items.get(i).getDisplayName());
-        }
+        PlacedOrder savedOrder = orderService.placeOrder(orderItems);
+        System.out.println(orderService.getOrderSummary(orderItems));
+        return String.format("Order placed successfully! Order ID: %d", savedOrder.getId());
     }
 
 }
