071c78c1ce7016685921e05d0a6866da9b5d5a68
Paul <paul.matros@gmail.com>
2025-01-20 00:51:09 +0100

    order renamed to PlacedOrder

--- src/main/java/pl/matros/restaurant/model/PlacedOrder.java
+++ src/main/java/pl/matros/restaurant/model/PlacedOrder.java
@@ -1,18 +1,16 @@
 package pl.matros.restaurant.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
-public class Order {
+public class PlacedOrder {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Lob
     private String items;
     private int totalPrice;
 }
